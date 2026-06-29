e4faac48bc177b8701ed95a3b8c25a0ae8c2d4eb
mikohan <angara99@gmail.com>
2026-06-29 13:34:28 -0700

    Sitemap config

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl:
-    process.env.NEXT_PUBLIC_COMPANY_WEBSITE || "https://angarasteamers.com", // Replace with your actual domain
-  generateRobotsTxt: true, // (Optional) Generates robots.txt
-  exclude: ["/test", "/admin"],
-};
